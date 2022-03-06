94639ba3c8be65308789df8b7f315b363813a54e
0nehundr3d <70420762+0nehundr3d@users.noreply.github.com>
2022-03-06 16:09:56 -0700

    add yiff command

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -1,17 +1,19 @@
 import nextcord
 from nextcord.ext import commands
-import Wrappers.e621Wrapper as e621
-
+from Wrappers.e621Wrapper import E621API
 
 class NSFW(commands.Cog):
 	def __init__(self, bot:commands.Bot):
-		self.bot = bot
-
-	e6Client = e621.E621API(user_agent="0ne/COFEFEBOTT")
+		self.bot = bot	
+		self.e6Client = E621API(user_agent="C0FEFEBOTT (by 0ne)")
 
 	@commands.command()
-	async def test(self, ctx):
-		await ctx.send(self.e6client.get_posts())
+	async def yiff(self, ctx, tags:str = "", limit:int = 1):
+		if not ctx.channel.nsfw: return await ctx.send("This command can only be used in NSFW channels.")
+		
+		posts = self.e6Client.get_posts(tags, limit)
+		for post in posts["posts"]:
+			await ctx.send(post["file"]["url"])
 
 def setup(bot:commands.Bot):
 	bot.add_cog(NSFW(bot))
