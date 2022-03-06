6c301b1dffeec072c4b8ba319c880ed54bcf0d84
0nehundr3d <70420762+0nehundr3d@users.noreply.github.com>
2022-03-06 16:10:21 -0700

    add tags and search limit to wrapper

--- Wrappers/e621Wrapper.py
+++ Wrappers/e621Wrapper.py
@@ -1,22 +1,22 @@
 import json
 import requests
 
-class E621API:
+class E621API(object):
 	def __init__(self, user_agent):
-		url_base = "https://e621.net/"
-		headers = {"Content-Type": "application/json", "User-Agent": user_agent}
+		self.url_base = "https://e621.net/"
+		self.headers = {"Content-Type": "application/json", "User-Agent": user_agent}
 
-		self.url_base = url_base
-		self.headers = headers
+	def get_posts(self, tags:str = "", limit:int = 1):
+		tags = tags.replace(" ", "+")
+		if limit > 10: limit = 10
 
-	def get_posts(self):
-		url = f"{self.url_base}post.json"
+		url = f"{self.url_base}posts.json?limit={limit}&tags={tags}+status:active+rating:explicit"
 
 		response = requests.get(url, headers=self.headers)
 
 		if response.status_code == 200:
 			return json.loads(response.content.decode("utf-8"))
 		else:
-			return response.status_code
+			return f"{response.status_code}: {response.reason}"
 
 	
