6f41f3bfb0159afe36c7b1813e0200c69c435648
0nehundr3d <70420762+0nehundr3d@users.noreply.github.com>
2022-03-06 18:12:23 -0700

    add embed to nsfw commands

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -1,21 +1,25 @@
 import nextcord
 from nextcord.ext import commands
 from Wrappers.e621Wrapper import E621API
+import json
 
 class NSFW(commands.Cog):
 	def __init__(self, bot:commands.Bot):
 		self.bot = bot	
 		self.e6Client = E621API(user_agent="C0FEFEBOTT (by 0ne)")
 
+	with open("config/configuration.json", "r") as f:
+		manager = json.load(f)["manager"]
+
 	@commands.command()
 	async def yiff(self, ctx, tags:str = "", limit:int = 1):
-		#if not ctx.channel.nsfw: return await ctx.send("This command can only be used in NSFW channels.")
+		if not ctx.channel.nsfw and ctx.author.id not in self.manager: return await ctx.send("This command can only be used in NSFW channels.")
 		
 		posts = self.e6Client.get_posts(tags, limit)
 		for post in posts["posts"]:
 			postUrl= post["file"]["url"]
 
-			embed = nextcord.Embed(title="link",url=post["sources"][0],thumbnail=postUrl)
+			embed = nextcord.Embed(title="Source",url=post["sources"][0]).set_image(url=postUrl)
 			await ctx.send(embed=embed)
 
 def setup(bot:commands.Bot):
