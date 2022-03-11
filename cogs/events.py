df296a551dcfada9505a3b9825a95424d3c9b2e9
0nehundr3d <70420762+0nehundr3d@users.noreply.github.com>
2022-03-11 16:21:58 -0700

    add bullyMutt command

--- cogs/events.py
+++ cogs/events.py
@@ -7,6 +7,7 @@ class Events(commands.Cog):
 		self.bot = bot
 
 	with open("config/exemptions.json", "r") as f: exemptions = json.load(f)["exemptions"]
+	with open("config/exemptions.json", "r") as f: harass = json.load(f)["muttHarass"]
 
 	with open("config/configuration.json", "r") as f: manager = json.load(f)["manager"]
 
@@ -57,15 +58,34 @@ class Events(commands.Cog):
 			else:
 				await ctx.reply(f"{user.name} is not exempted.")
 
+	@commands.command()
+	async def bullyMutt(self, ctx):
+		with open("config/exemptions.json", "r") as f: config = json.load(f)
+
+		if self.harass:
+			config["muttHarass"] = False
+			with open("config/exemptions.json", "w") as f:
+				json.dump(config, f, indent=4)
+			await ctx.send("Mutt harassment is now disabled.")
+			self.bot.reload_extension("cogs.events")
+		else:
+			config["muttHarass"] = True
+			with open("config/exemptions.json", "w") as f:
+				json.dump(config, f, indent=4)
+			await ctx.send("Mutt harassment is now enabled.")
+			self.bot.reload_extension("cogs.events")
 
 	@commands.Cog.listener()
 	async def on_message(self, msg):
 		if msg.author.id in self.exemptions: return
 
-		msg = f" {msg} "
+		msgstring = f" {msg.content.lower()} "
 
-		if " horny " in msg.content.lower(): await msg.add_reaction("<:gay:898076464061231125>")
-		if " cum " in msg.content.lower(): await msg.add_reaction("<:cum:950228416135843901>")
+		if self.harass and msg.author.id == 525896357756796948: await msg.channel.send("<:CheemWierd:951980915960184842> ")
+		if " horny " in msgstring: await msg.add_reaction("<:gay:898076464061231125>")
+		if " cum " in msgstring: await msg.add_reaction("<:cum:950228416135843901>")
+		if " bruh " in msgstring: await msg.add_reaction("<a:catdie:951702853817360394>")
+
 
 def setup(bot:commands.Bot):
 	bot.add_cog(Events(bot))
