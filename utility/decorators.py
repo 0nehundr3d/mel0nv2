2616b3ffbab83cbf599df8c3f2f39df182baa26a
0nehundr3d <70420762+0nehundr3d@users.noreply.github.com>
2022-05-07 10:13:15 -0600

    remove unused method

--- utility/decorators.py
+++ utility/decorators.py
@@ -6,9 +6,6 @@ class Decorators:
 	def __init__(self):
 		with open("config/configuration.json", "r") as f: self.config = json.load(f)
 
-	def reloadJsons(self):
-		with open("config/configuration.json", "r") as f: self.config = json.load(f)
-
 	def is_manager(self):
 		def preticate(ctx):
 			return ctx.author.id in self.config["manager"]
