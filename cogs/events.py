94fdcc21e4d4812c83c38551f87c783bb6178065
0nehundr3d <70420762+0nehundr3d@users.noreply.github.com>
2022-05-07 10:37:34 -0600

    add mutt and neo bully commands

--- cogs/events.py
+++ cogs/events.py
@@ -2,17 +2,20 @@ import nextcord
 from nextcord.ext import commands
 import json
 import utility.decorators as decorators
+import typing
 
 class Events(commands.Cog):
 	def __init__(self, bot:commands.Bot):
 		self.bot = bot
+
+		self.muttHarass = False
+		self.neoHarass = False
 
 	Decorators = decorators.Decorators()
 
 	with open("config/exemptions.json", "r") as f:
 		e = json.load(f) 
 		exemptions = e["exemptions"]
-		harass = e["muttHarass"]
 
 	@commands.command()
 	@Decorators.is_manager()
@@ -63,23 +66,19 @@ class Events(commands.Cog):
 
 	@commands.command()
 	@Decorators.is_manager()
-	async def bullyMutt(self, ctx):
-
-		with open("config/exemptions.json", "r") as f: config = json.load(f)
-
-		if self.harass:
-			config["muttHarass"] = False
-			with open("config/exemptions.json", "w") as f:
-				json.dump(config, f, indent=4)
-			await ctx.send("Mutt harassment is now disabled.")
-			self.bot.reload_extension("cogs.events")
-		else:
-			config["muttHarass"] = True
-			with open("config/exemptions.json", "w") as f:
-				json.dump(config, f, indent=4)
+	async def bully(self, ctx, user:typing.Literal["mutt", "neo"]):
+		if user == "mutt" and not self.muttHarass:
 			await ctx.send("Mutt harassment is now enabled.")
-			await ctx.guild.get_member(525896357756796948).send(":)")
-			self.bot.reload_extension("cogs.events")
+			self.muttHarass = True
+		elif user == "mutt" and self.muttHarass:
+			await ctx.send("Mutt harassment is now disabled.")
+			self.muttHarass = False
+		elif user == "neo" and not self.neoHarass:
+			await ctx.send("Neo harassment is now enabled.")
+			self.neoHarass = True
+		elif user == "neo" and self.neoHarass:
+			await ctx.send("Neo harassment is now disabled.")
+			self.neoHarass = False
 
 	@commands.Cog.listener()
 	async def on_message(self, msg):
@@ -89,8 +88,8 @@ class Events(commands.Cog):
 		for item in ['.',',','!','?']:
 			msgstring = msgstring.replace(item, "")
 
-		if self.harass and msg.author.id == 972241312805970061: await msg.channel.send("<:CheemWierd:951980915960184842>")
-		#if msg.author.id == 555045424222830594: await msg.channel.send(msg.content)
+		if self.muttHarass and msg.author.id == 972241312805970061: await msg.channel.send("<:CheemWierd:951980915960184842>")
+		if self.neoHarass and msg.author.id == 972241312805970061 and "cope" in msg.content: await msg.channel.send("xope")
 		if " horny " in msgstring: await msg.add_reaction("<:gay:898076464061231125>")
 		if " cum " in msgstring: await msg.add_reaction("<:cum:950228416135843901>")
 		if " bruh " in msgstring: await msg.add_reaction("<a:catdie:951702853817360394>")
