584ac85dbc294ccfa5312a810efdb8016c0b05f1
0nehundr3d <70420762+0nehundr3d@users.noreply.github.com>
2022-05-07 10:21:18 -0600

    add say command

--- cogs/pain.py
+++ cogs/pain.py
@@ -1,12 +1,16 @@
-import nextcord
 from nextcord.ext import commands
-
+import nextcord
+import typing
 
 class Pain(commands.Cog):
 	def __init__(self, bot:commands.Bot):
 		self.bot = bot
 
-	
+	@commands.command()
+	async def say(self, ctx,channel: typing.Optional[nextcord.TextChannel], *, message:str):
+		if channel is None:
+			channel = ctx.channel
+		await channel.send(message)
 
 def setup(bot:commands.Bot):
 	bot.add_cog(Pain(bot))
