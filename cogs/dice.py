8c00e279c86ec2d79d6d4fe1bb66836366739fa4
0nehundr3d <70420762+0nehundr3d@users.noreply.github.com>
2022-07-11 12:42:21 -0600

    made roll command not bad

--- cogs/dice.py
+++ cogs/dice.py
@@ -1,8 +1,10 @@
-from multiprocessing.sharedctypes import Value
+from operator import mod
 import random
 from nextcord.ext import commands
 import random
 import re
+
+from regex import P
 
 class CogName(commands.Cog):
 	def __init__(self, bot:commands.Bot):
@@ -10,20 +12,51 @@ class CogName(commands.Cog):
 
 	@commands.command()
 	async def roll(self, ctx, dice:str):
-		nums = re.split("d|\+", dice)
+		die = {
+			"ammount" : 0,
+			"sides" : 0,
+			"mod" : 0
+		}
+
+		die["ammount"] = int(dice[:dice.index(re.search("d", dice).group())])
+
+		dice = dice.replace(dice[:dice.index(re.search("d", dice).group()) + 1], '', 1)
+
 		try:
-			pNums = [int(num) for num in nums]
-		except ValueError:
-			await ctx.send("Please make sure you are using the correct dice format")
-			print(nums)
-			return
-		results = [random.randint(1,pNums[1]) for i in range(pNums[0])]
+			die["sides"] = int(dice[:dice.index(re.search("\D", dice).group())])
 
-		mod = 0
-		if len(pNums) == 3:
-			mod = pNums[2]
+			dice = dice.replace(dice[:dice.index(re.search("\D", dice).group())], '', 1)
+		except:
+			die["sides"] = int(dice)
+			dice = ''
+			
+		while re.search("[+-]", dice):
+			v = re.search("[+-]", dice).group()
 
-		await ctx.send(f"{results} total:{sum(results) + mod}")
+			if v == "+":
+				dice = dice.replace('+', '', 1)
+				try:
+					die["mod"] += int(dice[:dice.index(re.search("\D", dice).group())])
+					dice = dice.replace(dice[:dice.index(re.search("\D", dice).group())], '', 1)
+				except:
+					die["mod"] += int(dice)
+					dice = ''
+
+			elif v == "-":
+				dice = dice.replace('-', '', 1)
+				try:
+					die["mod"] -= int(dice[:dice.index(re.search("\D", dice).group())])
+					dice = dice.replace(dice[:dice.index(re.search("\D", dice).group())], '', 1)
+				except:
+					die["mod"] -= int(dice)
+					dice = ''
+
+		rolls = []
+
+		for i in range(die["ammount"]):
+			rolls.append(random.randint(1,die["sides"]))
+
+		await ctx.send(f"{rolls} total: {sum(rolls) + die['mod']}")
 
 
 def setup(bot:commands.Bot):
