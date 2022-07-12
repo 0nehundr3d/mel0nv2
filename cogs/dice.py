d5103798cb46c348ee5375f22858d7743d5acc1a
0nehundr3d <70420762+0nehundr3d@users.noreply.github.com>
2022-07-11 19:17:27 -0600

    add multi group rolling

--- cogs/dice.py
+++ cogs/dice.py
@@ -2,13 +2,13 @@ import random
 from nextcord.ext import commands
 import random
 import re
+import typing
 
 class CogName(commands.Cog):
 	def __init__(self, bot:commands.Bot):
 		self.bot = bot
 
-	@commands.command()
-	async def roll(self, ctx, dice:str):
+	def parseDice(self, dice : str):
 		die = {
 			"ammount" : 0,
 			"sides" : 0,
@@ -74,11 +74,10 @@ class CogName(commands.Cog):
 		removed = []
 
 		if set(range(1, die["sides"]+1, 1)).issubset(set(die["reroll"])):
-			return await ctx.send("Can not reroll all dice states")
+			return "Can not reroll all sides"
 
 		if die["ammount"] <= die["remove"]:
-			return await ctx.send("Can not remove all rolls")
-		
+			return "Can not remove all dice"
 
 		for i in range(die["ammount"]):
 			roll = random.randint(1,die["sides"])
@@ -89,9 +88,22 @@ class CogName(commands.Cog):
 		for i in range(die["remove"]):
 			removed.append(rolls.pop(rolls.index(min(rolls))))
 
-		toSend = f"{rolls} total: {sum(rolls) + die['mod']}"
+		parsedStr = f"{rolls} total: {sum(rolls) + die['mod']}"
 		if len(removed) > 0:
-			toSend = toSend + f" (removed {removed})"
+			parsedStr = parsedStr + f" (removed {removed})"
+
+		return parsedStr
+
+	@commands.command()
+	async def roll(self, ctx, ammount: typing.Optional[int] = 1 ,dice:str = ""):
+		
+		toSend = ""
+
+		if dice:
+			for i in range(ammount):
+				toSend += self.parseDice(dice) + "\n"
+		else:
+			return await ctx.send("Please enter a dice string to parse")
 
 		await ctx.send(toSend)
 
