5da26c7ca2bb93845be97c503da5bbcb12a17c39
0nehundr3d <70420762+0nehundr3d@users.noreply.github.com>
2022-07-11 18:31:55 -0600

    remove possible loop from reroll

--- cogs/dice.py
+++ cogs/dice.py
@@ -14,7 +14,8 @@ class CogName(commands.Cog):
 		die = {
 			"ammount" : 0,
 			"sides" : 0,
-			"mod" : 0
+			"mod" : 0,
+			"reroll" : []
 		}
 
 		die["ammount"] = int(dice[:dice.index(re.search("d", dice).group())])
@@ -28,9 +29,11 @@ class CogName(commands.Cog):
 		except:
 			die["sides"] = int(dice)
 			dice = ''
+
+		detectStr = "[+-rk]"
 			
-		while re.search("[+-]", dice):
-			v = re.search("[+-]", dice).group()
+		while re.search(detectStr, dice):
+			v = re.search(detectStr, dice).group()
 
 			if v == "+":
 				dice = dice.replace('+', '', 1)
@@ -50,10 +53,25 @@ class CogName(commands.Cog):
 					die["mod"] -= int(dice)
 					dice = ''
 
+			elif v == "r":
+				dice = dice.replace("r", '', 1)
+				try:
+					die["reroll"].append(int(dice[:dice.index(re.search("\D", dice).group())]))
+					dice = dice.replace(dice[:dice.index(re.search("\D", dice).group())], '', 1)
+				except:
+					die["reroll"].append(int(dice))
+					dice = ''
+
 		rolls = []
 
+		if set(range(1, die["sides"]+1, 1)).issubset(set(die["reroll"])):
+			return await ctx.send("can not reroll all dice states")
+
 		for i in range(die["ammount"]):
-			rolls.append(random.randint(1,die["sides"]))
+			roll = random.randint(1,die["sides"])
+			while roll in die["reroll"]:
+				roll = random.randint(1,die["sides"])
+			rolls.append(roll)
 
 		await ctx.send(f"{rolls} total: {sum(rolls) + die['mod']}")
 
